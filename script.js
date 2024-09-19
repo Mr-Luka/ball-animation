b47998a6c10afcec6da9f2e2770ed5bc385dba6c
Luka <luka.luka.luka93@gmail.com>
2024-09-19 12:08:15 -0700

    selected the buttons and the ball, made a variable for the roll animation, roll options and animated the ball using animate()

---
+++ script.js
@@ -0,0 +1,27 @@
+const ball = document.querySelector("#ball");
+const play = document.querySelector("#play");
+const pause = document.querySelector("#pause");
+const reverse = document.querySelector("#reverse");
+const speedUp = document.querySelector("#speed-up");
+const speedDown = document.querySelector("#speed-down");
+
+const rollAnimation = [{
+    transform: 'rotate(0) translate3D(-50%, -50%, 0)',
+    color: "white",
+    },
+    {
+        color: 'blue',
+        offset: 0.3
+    },
+    {
+        transform: "rotate(360deg) translate3D(-50%, -50%, 0)",
+        color: "white",
+    }
+];
+
+const rollOptions = {
+    duration: 3000,
+    iterations: Infinity,
+}
+
+const roll = ball.animate(rollAnimation, rollOptions);
